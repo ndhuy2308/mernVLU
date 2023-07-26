890fa395b2c200c8aec7d7f4ef8e8bc927bedb6b
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2023-07-26 10:55:14 +0700

    #1

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.bmycfgk.mongodb.net',
+    DB_USER: 'ndhuy2308',
+    DB_PASS: 'mongopassword_2308',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'huypero114@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'qnzlccnvjartdilv',
+    JWT_SECRET: 'huy',
+    JWT_EXPIRES: '1800000', // in milliseconds
+  };
+  module.exports = MyConstants;
